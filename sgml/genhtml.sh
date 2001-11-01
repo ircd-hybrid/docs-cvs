6f37b0c78cccc967b39ee189bca4e6450d841221
wcampbel <wcampbel>
2001-11-01 00:56:38 +0000

    Rewrote INSTALL for Hybrid 7 into SGML, output is located on   http://kabel.pp.ru/sgml/ if anyone wants to see. Corrected an issue with the genhtml.sh script. Generate the .txt automatically with the genhtml.sh script.

--- sgml/genhtml.sh
+++ sgml/genhtml.sh
@@ -10,3 +10,12 @@ jade -V nochunks -c /usr/local/share/sgml/docbook/dsssl/modular/catalog \
   -t sgml $1.sgml >$1.html
 
 tidy -m $1.html
+
+# Also generate the text version
+links -dump $1.html >$1.txt.tmp
+
+# links will sometimes put in a NUL character into the output, which is
+# fine for most UNIX editors and viewers, but some Windows ones puke on
+# it.  Just be safe and remove it
+tr -d '\000' <$1.txt.tmp >$1.txt
+rm $1.txt.tmp
